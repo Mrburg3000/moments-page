9d88949acc168fe9018c32e08fee4e44f36e53f0
João Santos <jm0703507@gmail.com>
2026-01-21 17:56:55 -0300

    Second commit

--- crystal-moments/src/app/app.routes.ts
+++ crystal-moments/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home';
+import { AboutComponent } from './components/pages/about/about';
+import { NewMoment } from './components/pages/new-moment/new-moment';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'moments/new', component: NewMoment }
+];
